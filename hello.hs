390e7d94c7784d0abe8a944b56d8e4208aa1867b
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2025-04-22 21:24:48 +0530

    Changes to be committed: 	modified:   hello.hs 	new file:   number.hs

--- hello.hs
+++ hello.hs
@@ -1,3 +1,7 @@
 main = do
   putStrLn "Hello, everybody!"
   putStrLn ("Please look at my favorite odd numbers: " ++ show (filter odd [10..20]))
+  --this is a comment 
+  {- this 
+  is a block 
+  comment-}
